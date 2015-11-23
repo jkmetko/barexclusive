a03c5dfa39ce90083e672598f7cb5a7c27380336
trickysoft-bris <bris.ladislav@gmail.com>
2015-11-23 17:06:54 +0100

    new index page with navigation

---
+++ js/starter-template.js
@@ -0,0 +1,30 @@
+/**
+ * Created by trickysoft-bris on 11/23/15.
+ */
+$(document).ready(function() {
+    function HoverNavMethod() {
+
+        if ($(window).width() > 993) {
+
+            $('ul.nav li.dropdown').hover(function () {
+                $(this).find('.dropdown-menu').stop(true, true).delay(80).fadeIn(400);
+            }, function () {
+                $(this).find('.dropdown-menu').stop(true, true).delay(80).fadeOut(400);
+            });
+        }
+        else{
+            $('ul.nav li.dropdown').hover(function () {
+                $(this).find('.dropdown-menu').stop(true, true);
+            }, function () {
+                $(this).find('.dropdown-menu').stop(true, true);
+            });
+        }
+    }
+
+    HoverNavMethod();
+
+    // bind resize event
+    $(window).resize(HoverNavMethod);
+
+});
+
